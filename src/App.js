881acc6aef33e8603ea7beb6166495dd77ca0d52
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-14 16:31:41 +0700

    setup SHop Page

--- src/App.js
+++ src/App.js
@@ -3,34 +3,14 @@ import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-	<div>
-		<h1>HATS PAGE</h1>
-	</div>
-);
-
-const JacketPage = () => (
-	<div>
-		<h1>jacket PAGE</h1>
-	</div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
 	return (
 		<div>
-			{/* exact artinya true atau false 
-		kalau true maka harus persis di path nya 
-		contoh - kalau pakai exact - halaman sebelumnya seprti hoempage tidak akan di render */}
-			{/* switch akan merender secara order -contoh di bwah aswitch akan merender homepage dulu baru yang lainya 
-			memberikan kontrol di code
-			jadi tidak akan accidently render multiple component
-			*/}
-
 			<Switch>
 				<Route exact path='/' component={HomePage} />
-				<Route path='/hats' component={HatsPage} />
-				<Route path='/jackets' component={JacketPage} />
+				<Route path='/shop' component={ShopPage} />
 			</Switch>
 		</div>
 	);

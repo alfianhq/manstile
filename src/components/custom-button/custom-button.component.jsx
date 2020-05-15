322ebb0af8fc2e514cd777fc51b9bcf3a8383895
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-15 11:30:06 +0700

    custom button

---
+++ src/components/custom-button/custom-button.component.jsx
@@ -0,0 +1,13 @@
+import React, {Children} from 'react';
+
+import './custom-button.styles.scss';
+
+const CustomButton = ({children, ...otherProps}) => {
+	return (
+		<button className='custom-button' {...otherProps}>
+			{children}
+		</button>
+	);
+};
+
+export default CustomButton;

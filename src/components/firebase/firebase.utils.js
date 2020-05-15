8c0a470e0c283112d3dc89ad7a8aa2dfdd669bf6
alfianhq <63658875+alfianhq@users.noreply.github.com>
2020-05-15 15:35:08 +0700

    storing data in firebase

--- src/components/firebase/firebase.utils.js
+++ src/components/firebase/firebase.utils.js
@@ -12,6 +12,33 @@ const config = {
 	appId: '1:1093474482701:web:cbbcf23b0b8aba5d32b344',
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	// apabila tidak ada atutentikasi .. exit
+	if (!userAuth) return;
+
+	// apabila ada
+	const userRef = firestore.doc(`user/${userAuth.uid}`);
+
+	const snapShot = await userRef.get();
+
+	if (!snapShot.exists) {
+		const {displayName, email} = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.log('error creating user ', error.message);
+		}
+	}
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
